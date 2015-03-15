190a251e9bcd198849faee9e63c83862c279a8df
Bart Addis <bart@addis.net>
2015-03-15 15:27:20 -0400

    Initial commit

---
+++ src/main/java/addis/net/TicTacToe.java
@@ -0,0 +1,41 @@
+package addis.net;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicTacToe {
+    private static final int WIDTH = 3;
+    private static final int HEIGHT = 3;
+    private int[][] board = new int[WIDTH][HEIGHT];
+    public TicTacToe() {
+    }
+
+    List<Integer> possibleMoves() {
+        List<Integer> rval = new ArrayList<Integer>();
+        for (int i=0; i<WIDTH*HEIGHT; i++) {
+            if (0==board[x(i)][y(i)]) {
+                 rval.add(i);
+            }
+        }
+        return rval;
+    }
+
+    int x(int i) {
+        if (i<0) {
+            return 0;
+        } else if (i>=WIDTH*HEIGHT) {
+            return WIDTH-1;
+        } else {
+            return i / WIDTH;
+        }
+    }
+    int y(int i) {
+        if (i<0) {
+            return 0;
+        } else if (i>=WIDTH*HEIGHT) {
+            return HEIGHT-1;
+        } else {
+            return i % HEIGHT;
+        }
+    }
+}
---
+++ src/test/java/addis/net/TicTacToeTest.java
@@ -0,0 +1,49 @@
+package addis.net;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class TicTacToeTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testCtor() {
+        TicTacToe t = new TicTacToe();
+        assertEquals(9, t.possibleMoves().size());
+    }
+
+    @Test
+    public void testXandY() {
+        TicTacToe t = new TicTacToe();
+        assertEquals(0,t.x(-1));
+        assertEquals(0,t.x(0));
+        assertEquals(0,t.x(1));
+        assertEquals(0,t.x(2));
+        assertEquals(1,t.x(3));
+        assertEquals(1,t.x(4));
+        assertEquals(1,t.x(5));
+        assertEquals(2,t.x(6));
+        assertEquals(2,t.x(7));
+        assertEquals(2,t.x(8));
+        assertEquals(2,t.x(9));
+
+        assertEquals(0,t.y(-1));
+        assertEquals(0,t.y(0));
+        assertEquals(1,t.y(1));
+        assertEquals(2,t.y(2));
+        assertEquals(0,t.y(3));
+        assertEquals(1,t.y(4));
+        assertEquals(2,t.y(5));
+        assertEquals(0,t.y(6));
+        assertEquals(1,t.y(7));
+        assertEquals(2,t.y(8));
+        assertEquals(2,t.y(9));
+    }
+
+}
